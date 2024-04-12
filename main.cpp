39a8c1efd4f476eef90ab2ab426c17e7355903b9
avillacampafiuba <avillacampa@fi.uba.ar>
2024-04-12 01:01:22 +0000

    Empezamos con el punto 11, tiene errores

--- main.cpp
+++ main.cpp
@@ -45,5 +45,45 @@ int main()
 
     #if (TEST_X == TEST_BUS)
 
+    //DigitalIn gasDetector(D2);
+    //DigitalIn overTempDetector(D3);
+    DigitalIn aButton(D4);
+    DigitalIn bButton(D5);
+    DigitalIn cButton(D6);
+    DigitalIn dButton(D7);
+
+    int gasDetectorMask = 0x00;
+    int overTempDetectorMask = 0x00;
+
+    PortIn detectors(PortA, gasDetectorMask & overTempDetectorMask);
+
+    BusIn buttons(aButton, bButton, cButton, dButton);
+
+    DigitalOut alarmLed(LED1);
+
+    gasDetector.mode(PullDown);
+    overTempDetector.mode(PullDown);
+    aButton.mode(PullDown);
+    bButton.mode(PullDown);
+    cButton.mode(PullDown);
+    dButton.mode(PullDown);
+
+    alarmLed = OFF;
+
+    bool alarmState = OFF;
+
+    while (true) {
+
+        if ( gasDetector || overTempDetector ) {
+            alarmState = ON;
+        }
+
+        alarmLed = alarmState;
+
+        if ( aButton && bButton && !cButton && !dButton) {
+            alarmState = OFF;
+        }
+    }
+
     #endif
 }
