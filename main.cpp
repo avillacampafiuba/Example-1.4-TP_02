a498cdcaf61624a329c873bb55113c1ca093cbe9
lburdman <67927684+lburdman@users.noreply.github.com>
2024-04-18 19:20:51 -0300

    Listo para probar en placa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+
 #define TEST_DIG 0 
 #define TEST_BUS 1 
 #define TEST_X (TEST_BUS) 
@@ -36,7 +37,7 @@ int main()
 
         alarmLed = alarmState;
 
-        if ( aButton && bButton && !cButton && !dButton) {
+        if ( aButton && bButton && !cButton && !dButton) { //1100 -> 12 -> 0xC
             alarmState = OFF;
         }
     }
@@ -44,46 +45,32 @@ int main()
     #endif
 
     #if (TEST_X == TEST_BUS)
+        //En este caso, podria ser conveniente usar PortIn para fasDetector y overTempDetector, pero dado que D2 y D3 no pertenecen al mismo puerto
+        //no es posible, una opcion seria cambiar los pines para que esten en ese puerto y simplificar la condicion de estas entradas.
 
-    //DigitalIn gasDetector(D2);
-    //DigitalIn overTempDetector(D3);
-    DigitalIn aButton(D4);
-    DigitalIn bButton(D5);
-    DigitalIn cButton(D6);
-    DigitalIn dButton(D7);
+        BusIn buttonBus(D4, D5, D6, D7); // Pines de los botones en un BusIn
+        DigitalIn gasDetector(D2);
+        DigitalIn overTempDetector(D3);
 
-    int gasDetectorMask = 0x00;
-    int overTempDetectorMask = 0x00;
+        DigitalOut alarmLed(LED1);
 
-    PortIn detectors(PortA, gasDetectorMask & overTempDetectorMask);
+        gasDetector.mode(PullDown);
+        overTempDetector.mode(PullDown);
 
-    BusIn buttons(aButton, bButton, cButton, dButton);
+        alarmLed = OFF;
 
-    DigitalOut alarmLed(LED1);
+        bool alarmState = OFF;
 
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
+        while (true) {
+            if (gasDetector || overTempDetector) {
+                alarmState = ON;
+            }
 
-    alarmLed = OFF;
+            alarmLed = alarmState;
 
-    bool alarmState = OFF;
-
-    while (true) {
-
-        if ( gasDetector || overTempDetector ) {
-            alarmState = ON;
+            if (buttonBus == 0xC) { // Si los botones son 1100  (0xC en hexa)
+                alarmState = OFF;
+            }
         }
-
-        alarmLed = alarmState;
-
-        if ( aButton && bButton && !cButton && !dButton) {
-            alarmState = OFF;
-        }
-    }
-
     #endif
 }
